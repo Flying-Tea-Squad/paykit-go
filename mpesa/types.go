e38015bc048d3b528638606e6ea2eb20b5cbfadc
ochola-rich <richardochola3@gmail.com>
2026-07-18 21:17:13 +0300

    task : added the B2C request , CB2Simulation and C@Bregister request

--- mpesa/types.go
+++ mpesa/types.go
@@ -22,3 +22,34 @@ type STKPushResponse struct {
 	ResponseDescription string `json:"ResponseDescription"`
 	CustomerMessage   string `json:"CustomerMessage"`
 }
+
+type C2BRegisterRequest struct {
+	IdempotencyKey string `json:"-"`
+	ShortCode string `json:"ShortCode"`
+	ResponseType string `json:"ResponseType"`
+	ConfirmationURL string `json:"ConfirmationURL"`
+	ValidationURL string `json:"ValidationURL"`
+}
+
+type CB2SimulateRequest struct {
+	IdempotencyKey string `json:"-"`
+	ShortCode string `json:"ShortCode"`
+	CommandID string `json:"CommandID"`
+	Amount int `json:"Amount"`
+	Msisdn string `json:"Msisdn"`
+	BillRefNumber string `json:"BillRefNumber"`
+}
+
+type B2CRequest struct {
+	IdempotencyKey string `json:"-"`
+	InitiatorName string `json:"InitiatorName"`
+	SecurityCredential string `json:"SecurityCredential"`
+	CommandID string `json:"CommandID"`
+	Amount int `json:"Amount"`
+	PartyA string `json:"PartyA"`
+	PartyB string `json:"PartyB"`
+	Remarks string `json:"Remarks"`
+	QueueTimeOutURL string `json:"QueueTimeOutURL"`
+	ResultURL string `json:"ResultURL"`
+	Occasion string `json:"Occasion,omitempty"`
+}
