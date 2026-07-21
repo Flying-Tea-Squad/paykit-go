d4600a101db0e675ff17af092ab1748171b38b6c
cokipruto <collinstunge005@gmail.com>
2026-07-21 12:14:55 +0300

    added generatePaswword helper

--- mpesa/signing.go
+++ mpesa/signing.go
@@ -1,1 +1,11 @@
 package mpesa
+
+import (
+	"encoding/base64"
+)
+
+func generatePassword(shortCode, passKey, timeStamp string) string {
+	// store the passwords into data
+	data := shortCode + passKey + timeStamp
+	return base64.StdEncoding.EncodeToString([]byte(data))
+}
