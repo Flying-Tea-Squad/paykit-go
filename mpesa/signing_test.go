47c6ff1a1c00b34b28bb51ad736b0552fca66dae
cokipruto <collinstunge005@gmail.com>
2026-07-21 13:10:09 +0300

    add unit test for password generator

---
+++ mpesa/signing_test.go
@@ -0,0 +1,21 @@
+package mpesa
+
+import (
+	"encoding/base64"
+	"testing"
+)
+
+//test file
+func TestGeneratePassword(t *testing.T) {
+	got := generatePassword(
+		"174379",
+		"abc123",
+		"202607211103045",
+	)
+	want := base64.StdEncoding.EncodeToString(
+		[]byte("174379abc123202607211103045"),
+	)
+	if got != want {
+		t.Errorf("expected %q, got %q", want ,got)
+	}
+}
