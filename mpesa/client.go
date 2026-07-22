8f45be4e09b7317aaaa98933292fba4b957fb933
gwanga <geremiwanga57@gmail.com>
2026-07-22 15:35:21 +0300

    feat: implement simple STK push endpoint request

--- mpesa/client.go
+++ mpesa/client.go
@@ -1,1 +1,39 @@
 package mpesa
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+type Client struct {
+	baseURL string
+	httpClient *http.Client
+
+}
+
+func (c *Client) STKPush(ctx context.Context, req STKPushRequest) (*STKPushRequest, error){
+
+	body, err := json.Marshal(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	httpReq, err := http.NewRequestWithContext(
+		ctx, 
+		http.MethodPost,
+		c.baseURL+"/mpesa/stkpush/v1/processrequest",
+		bytes.NewReader(body),
+	)
+
+	if err != nil {
+		return nil,err
+	}
+
+	_ = httpReq
+
+	return &STKPushResponse{}, nil
+	// req, err := http.NewRequest("POST", "/mpesa/stkpush/v1/processrequest", )
+}
