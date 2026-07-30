97890d6ea9d0c90c4b118c19faa54185428ac44a
mumutugi <wangibrian66@gmail.com>
2026-07-30 14:51:40 +0300

    fix(http): explicitly ignore non-critical return values for errcheck

--- http_test.go
+++ http_test.go
@@ -113,7 +113,7 @@ func TestHTTPClientDumpWriterAndLogger(t *testing.T) {
 	if err != nil {
 		t.Fatalf("do request: %v", err)
 	}
-	defer resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
